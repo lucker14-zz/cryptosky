d94e04169546e0ed41de8634455744b4ba9219b2
Petr Pluhar <petrpluhar@madebysource.com>
2017-09-27 18:58:25 +0200

    latest update

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,26 @@ $(document).ready(function(){
     }
 });
     
+
+var btn_showmore = document.querySelector('.roller .content button')
+var transparent = document.querySelector('.roadmap .transparent')
+var roller = document.querySelector('.roller')
+var roadmap_grid = document.querySelector('.roadmap-grid')
+var lines = [].slice.call(document.querySelectorAll('.roadmap-line'))
+
+console.log(btn_showmore)
+console.log(transparent)
+console.log(roller)
+console.log(lines)
+
+function showAll(){
+    transparent.classList.add('hidden')
+    roller.classList.add('hidden')
+    lines.forEach(function(item, index){
+        if (item.classList.contains('hidden')) 
+            item.classList.remove('hidden')
+    })
+    roadmap_grid.style.marginBottom = '70px';
+}
+
+btn_showmore.addEventListener('click', showAll)

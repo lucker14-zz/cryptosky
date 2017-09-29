1f9e45ce4f637054c81ae248924ba2a0fdeb98c1
Petr Pluhar <petrpluhar@madebysource.com>
2017-09-29 13:35:44 +0200

    split email popups

--- src/js/main.js
+++ src/js/main.js
@@ -20,8 +20,9 @@ btn_showmore.addEventListener('click', showAll)
 
 // contact form popup
 var open_btns = [].slice.call(document.querySelectorAll('.container--box button.btn'))
+var popups = [].slice.call(document.querySelectorAll('.popup-container'))
 var popup_bg = document.querySelector('.popup-bg')
-var close_btn = document.querySelector('.popup-container .close')
+var close_btns = [].slice.call(document.querySelectorAll('.popup-container .close'))
 
 function openModal(){
     if (popup_bg.classList.contains('hidden'))
@@ -37,11 +38,33 @@ function closeModal(){
         popup_bg.classList.add('disabled')
 }
 
+function swtichPopups(ele_id){
+    popups.forEach(function(item, index){
+        if (item.id == ele_id){
+            if (item.classList.contains('hidden'))
+                item.classList.remove('hidden')
+            if (item.classList.contains('disabled'))
+                item.classList.remove('disabled')
+        } else {
+            if (!item.classList.contains('hidden'))
+                item.classList.add('hidden')
+            if (!item.classList.contains('disabled'))
+                item.classList.add('disabled')
+        }
+    })
+}
+
 open_btns.forEach(function(item, index){
-    item.addEventListener('click', openModal)
+    item.addEventListener('click', function(e){
+        openModal()
+        console.log(e.target.id)
+        swtichPopups(e.target.id)
+    })
 })
 
-close_btn.addEventListener('click', closeModal)
+close_btns.forEach(function(item, index){
+    item.addEventListener('click', closeModal)
+})
 
 popup_bg.addEventListener('click', function(e){
     if (e.target.classList.contains('popup-bg')) 
